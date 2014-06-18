06c7d42e0c7f4f8917c826d43950cf0c0616a1d6
Alex <rockbean3000@gmail.com>
2014-06-18 21:53:36 +0800

    添加makeCacheMatrix和cacheSolve函数及相关注释
    coursera作业二内容

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################
+#
+# File: cachematrix.R
+# Description: Compute the inverse of a matrix and cache that
+# Date: 18/6/2014
+# Author: CaoXu (rockbean3000@gmail.com)
+# Test sample: 
+# >sample<-makeCacheMatrix()
+# >sample$set(matrix(c(1,0,2,4),2,2))
+# >sample$get()
+# >cacheSolve(sample)
+# >sample$getCache()
+################################################
 
-## Write a short comment describing this function
+# makeCacheMatrix - Create a vector, which is a list containing a function to cache a Matrix 
+# @x: The data matrix
+# Returns: A list contains functions: set, get, setCache, getCache
+# 1) set, set up the data matrix and cache matrix
+# 2) get, get the data matrix
+# 3) setCache, set the matrix into cache
+# 4) getCache, get the matrix from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # cache initialize
+  cache <- NULL
+  # set, data and cache matrix initialization
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  # get, only return data matrix
+  get <- function() x
+  # setCache, set the matrix into cache
+  setCache <- function(matrix) cache <<- matrix
+  # getCache, only return cache
+  getCache <- function() cache
+  # return the list filled with the functions
+  list(set = set, get = get,
+       setCache = setCache,
+       getCache = getCache)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve - Compute the inverse of a matrix and cache that
+# @x: The vector created by makeCacheMatrix
+# Returns: Either the cached matrix or the inverse of the data matrix if there isn't a cached one
+# Caution: The data matrix must be a square invertible one
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## If there isn't a cached matrix, it will compute the inverse of 
+  ## the matrix and set the result to the cache
+  if(is.null(x$getCache())) {
+    x$setCache(solve(x$get()))
+  }
+  message("getting inversed matrix")
+  x$getCache()
 }
